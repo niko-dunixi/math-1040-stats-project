e5eb6212460eb133f5823277211432e3c5d62157
Paul Baker <paul.baker@wgu.edu>
2017-04-16 15:13:33 -0600

    Generates a pie-chart
    Simple and rudimentary. Doesn't help with perception due to the fact the
    color label doesn't match the visual color on the pie-chart. We need to
    fix this.

--- term-project.r
+++ term-project.r
@@ -1,3 +1,23 @@
 #!/usr/bin/env rscript
 
-skittles = read.csv("skittle-data.csv")
+skittles = read.csv("skittle-data.csv", header = TRUE)
+columnTotals = colSums(skittles, na.rm=FALSE, dims = 1)
+rowTotals = rowSums(skittles, na.rm=FALSE, dims = 1)
+
+# Not suitable for labels, due to lowercasing 
+skittle_colors = c('red', 'orange', 'yellow', 'green', 'purple')
+
+
+print("Column Totals")
+print(columnTotals)
+
+print("Row Totals")
+print(rowTotals)
+
+piePercentages <- round(100 * columnTotals/ sum(columnTotals), 1)
+
+png(file="pie.png")
+pie(columnTotals, labels = piePercentages, col=rainbow(length(columnTotals)))
+legend("topright", c('Red','Orange','Yellow','Green','Purple'), cex = 0.8, fill = rainbow(length(columnTotals)))
+#pie(columnTotals)
+dev.off()
