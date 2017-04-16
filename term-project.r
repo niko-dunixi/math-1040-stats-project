13355ac2cdf0aebf19475d8227d0bb49392d4f1e
Paul Baker <paul.baker@wgu.edu>
2017-04-16 15:16:11 -0600

    Fixes the coloring of the pie-chart.
    Removes some dissonance when looking at the chart.

--- term-project.r
+++ term-project.r
@@ -4,7 +4,7 @@ skittles = read.csv("skittle-data.csv", header = TRUE)
 columnTotals = colSums(skittles, na.rm=FALSE, dims = 1)
 rowTotals = rowSums(skittles, na.rm=FALSE, dims = 1)
 
-# Not suitable for labels, due to lowercasing 
+# Not suitable for labels, due to lowercasing
 skittle_colors = c('red', 'orange', 'yellow', 'green', 'purple')
 
 
@@ -17,7 +17,7 @@ print(rowTotals)
 piePercentages <- round(100 * columnTotals/ sum(columnTotals), 1)
 
 png(file="pie.png")
-pie(columnTotals, labels = piePercentages, col=rainbow(length(columnTotals)))
-legend("topright", c('Red','Orange','Yellow','Green','Purple'), cex = 0.8, fill = rainbow(length(columnTotals)))
+pie(columnTotals, labels = piePercentages, col=skittle_colors)
+legend("topright", c('Red','Orange','Yellow','Green','Purple'), cex = 0.8, fill = skittle_colors)
 #pie(columnTotals)
 dev.off()
