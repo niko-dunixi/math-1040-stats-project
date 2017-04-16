b583c1c540c69acbc45e607df3f24193dde053ca
Paul Baker <paul.baker@wgu.edu>
2017-04-16 16:07:49 -0600

    Removing extra "dims = 1" from summation functions
    Removing this parameter gives us the correct answer for total skittles.
    The answer should be 1449, but I was off by one getting 1450. It was
    interpreting the dim=1 parameter on the sum() method as a vector to
    include, hence why it added an extra 1.
    
    http://stackoverflow.com/questions/43442484/r-sum-of-rowsums-off-by-one

--- term-project.r
+++ term-project.r
@@ -1,23 +1,25 @@
 #!/usr/bin/env rscript
 
 skittles = read.csv("skittle-data.csv", header = TRUE)
-columnTotals = colSums(skittles, na.rm=FALSE, dims = 1)
-rowTotals = rowSums(skittles, na.rm=FALSE, dims = 1)
-
-# Not suitable for labels, due to lowercasing
-skittle_colors = c('red', 'orange', 'yellow', 'green', 'purple')
-
+columnTotals = colSums(skittles)
+rowTotals = rowSums(skittles)
+total = sum(rowTotals)
 
 print("Column Totals")
 print(columnTotals)
 
-print("Row Totals")
+print("Row Totals (individual bags)")
 print(rowTotals)
+
+print('Total skittles')
+print(total)
+
+# Not suitable for labels, due to lowercasing
+skittle_colors = c('red', 'orange', 'yellow', 'green', 'purple')
 
 piePercentages <- round(100 * columnTotals/ sum(columnTotals), 1)
 
 png(file="pie.png")
 pie(columnTotals, labels = piePercentages, col=skittle_colors)
 legend("topright", c('Red','Orange','Yellow','Green','Purple'), cex = 0.8, fill = skittle_colors)
-#pie(columnTotals)
 dev.off()
